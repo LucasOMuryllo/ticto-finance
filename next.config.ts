f46eba7c6b429b22dd7368a4ebe2fb33cdee32e7
Lucas Muryllo <231128101+equipemodcar-ops@users.noreply.github.com>
2025-10-27 08:53:16 -0300

    criando build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+
+  output: "export",
+
+  images: {
+    unoptimized: true,
+  },
+
+  basePath: "/ticto-finance",
+  assetPrefix: "/ticto-finance/",
 };
 
 export default nextConfig;
